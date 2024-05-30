5286308a58f2585075d4c714dae5099e00e4c6f0
36018 <36018@ma-web.nl>
2024-05-30 14:12:10 +0200

    Dialogue video 1-6

---
+++ Narashima/Assets/Scripts/DialogueSystem/Response.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[System.Serializable]
+
+public class Response
+{
+    [SerializeField] private string responseText;
+    [SerializeField] private DialogueObject dialogueObject;
+
+    public string ResponseText => responseText;
+
+    public DialogueObject DialogueObject => dialogueObject;
+}
