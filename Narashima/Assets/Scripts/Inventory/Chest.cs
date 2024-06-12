a4aa6b51e8fdcb2a36e24245e5af56bfd77dc9d2
36018 <36018@ma-web.nl>
2024-06-12 10:22:21 +0200

    Inventory

---
+++ Narashima/Assets/Scripts/Inventory/Chest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Die();
+        }
+    }
+
+    private void Die() 
+    { 
+        GetComponent<LootBag>().InstantiateLoot(transform.position); 
+        Destroy(gameObject); 
+    }
+    
+}
